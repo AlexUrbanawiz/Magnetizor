c23ba97d23cf05e153badfdad39df643409ef2dc
AlexUrbanawiz <koalapokemonfun@gmail.com>
2026-01-22 08:57:06 -0700

    Work on magnet system

--- scenes/components/magnet_component.gd
+++ scenes/components/magnet_component.gd
@@ -5,8 +5,17 @@ extends Node
 @export var charge: float = 10.0;
 @export var magnetic_field: float = 10.0;
 
+var polarity: bool = false
+
 #True polarity - push
 #False polarity - pull
-func handle_magnetism(body: CharacterBody2D, polarity: bool) -> void:
+func handle_magnetism(body1: CharacterBody2D, body2: RigidBody2D) -> void:
 	if polarity:
-		
+			var direction = body1.direction_to(body2.global_position)
+			print(direction)
+			
+func swap_polarity() -> void:
+	if Input.is_action_just_pressed("swap_polarity"):
+		polarity = !polarity
+
+func shoot_magnet(body: CharacterBody2D)
